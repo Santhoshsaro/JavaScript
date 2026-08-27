8eb63a0459013df8f494db2e712e5f155dc9f7fb
SANTHOSHKUMAR <santhoshsaro2003@gmail.com>
2026-08-27 19:59:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Task 1:
+let studentName = "Karthik";
+let studentAge = 20;
+let studentMark = 85;
+console.log("Name:", studentName);
+console.log("Age:", studentAge);
+console.log("Mark:", studentMark);
+
+// Task 2:
+let userName = prompt("Enter your name:");
+alert("Your name is: " + userName);
+
+// Task 3: 
+let userAge = prompt("Enter your age:");
+console.log("Your age is:", userAge);
+
+// Task 4: 
+let userConfirm = confirm("Are you a student?");
+console.log("User confirmed:", userConfirm);
+
+// Task 5: 
+let nameForPage = prompt("Enter your name to display on page:");
+document.writeln("<h3>Welcome, " + nameForPage + "!</h3>");
