3b7a40e9ab0b1c5e0712bdbda2e36a7840833c99
SANTHOSHKUMAR <santhoshsaro2003@gmail.com>
2026-08-31 20:16:07 +0530

    Added JS Task2

---
+++ script1.js
@@ -0,0 +1,35 @@
+//TASK 1:
+
+var studentName = "Naveen";
+let studentAge = 22;
+const collegeName = "JJ College";
+
+console.log(studentName);
+console.log(studentAge);
+console.log(collegeName);
+
+// Change var
+studentName = "Kumar";
+console.log(studentName);
+
+// Change let
+studentAge = 23;
+console.log(studentAge);
+
+// Change Consts
+collegeName="Nehru College";
+console.log(collegeName);
+
+//TASK 2:
+
+let name = prompt("Enter your name:");
+let age=prompt("Enter your age");
+let blood=prompt("Enter your blood");
+
+console.log("Name:", name);
+console.log("Age:",age);
+console.log("blood group:",blood);
+
+// console.log("Age:", age);
+// console.log("City:", city);
+
